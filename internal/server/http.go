25abe4765c9c0424cc2d4fb8d3014fcdef2b5ba5
t1732 <oo1732oo@gmail.com>
2023-04-08 19:47:35 +0900

    refactor: handler

--- internal/server/http.go
+++ internal/server/http.go
@@ -10,41 +10,59 @@ import (
 	"gorm.io/gorm"
 )
 
+type Impl interface {
+	root(c echo.Context) error
+	mails(c echo.Context) error
+}
+
+type Handler struct {
+	db *gorm.DB
+}
+
+func newHandler(db *gorm.DB) Impl {
+	return &Handler{db: db}
+}
+
 func HttpNewServer(db *gorm.DB) *echo.Echo {
 	e := echo.New()
 	e.Logger.SetLevel(log.INFO)
 	e.HideBanner = true
 
-	e.GET("/", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, "Pong")
-	})
+	impl := newHandler(db)
 
-	e.GET("/mails", func(c echo.Context) error {
-		mails := []mdb.Mail{}
-		db.
-			Select("id", "created_at", "subject", "text", "html").
-			Preload("FromAddresses").
-			Preload("ToAddresses").
-			Preload("CcAddresses").
-			Preload("BccAddresses").
-			Find(&mails)
-
-		res := make([]response.Mail, len(mails))
-		for i, e := range mails {
-			res[i] = response.Mail{
-				CreatedAt:     e.CreatedAt,
-				FromAddresses: response.ConvertToMailAddresses(e.FromAddresses),
-				ToAddresses:   response.ConvertToMailAddresses(e.ToAddresses),
-				CcAddresses:   response.ConvertToMailAddresses(e.CcAddresses),
-				BccAddresses:  response.ConvertToMailAddresses(e.BccAddresses),
-				Subject:       e.Subject,
-				Text:          e.Text,
-				HTML:          e.HTML,
-			}
-		}
-
-		return c.JSON(http.StatusOK, res)
-	})
+	e.GET("/", impl.root)
+	e.GET("/mails", impl.mails)
 
 	return e
+}
+
+func (h *Handler) root(c echo.Context) error {
+	return c.JSON(http.StatusOK, "Pong")
+}
+
+func (h *Handler) mails(c echo.Context) error {
+	mails := []mdb.Mail{}
+	h.db.
+		Select("id", "created_at", "subject", "text", "html").
+		Preload("FromAddresses").
+		Preload("ToAddresses").
+		Preload("CcAddresses").
+		Preload("BccAddresses").
+		Find(&mails)
+
+	res := make([]response.Mail, len(mails))
+	for i, e := range mails {
+		res[i] = response.Mail{
+			CreatedAt:     e.CreatedAt,
+			FromAddresses: response.ConvertToMailAddresses(e.FromAddresses),
+			ToAddresses:   response.ConvertToMailAddresses(e.ToAddresses),
+			CcAddresses:   response.ConvertToMailAddresses(e.CcAddresses),
+			BccAddresses:  response.ConvertToMailAddresses(e.BccAddresses),
+			Subject:       e.Subject,
+			Text:          e.Text,
+			HTML:          e.HTML,
+		}
+	}
+
+	return c.JSON(http.StatusOK, res)
 }
