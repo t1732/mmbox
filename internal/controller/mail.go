b3c60b4fa18c1a8600549ae5023f0d632625d7f0
t1732 <oo1732oo@gmail.com>
2023-04-10 19:04:52 +0900

    refactor: handler を controller に

---
+++ internal/controller/mail.go
@@ -0,0 +1,50 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+
+	mdb "github.com/t1732/mmbox/internal/model/db"
+	"github.com/t1732/mmbox/internal/model/response"
+)
+
+type mailImpl interface {
+	Index(c echo.Context) error
+}
+
+type mailController struct {
+	db *gorm.DB
+}
+
+func NewMialController(db *gorm.DB) mailImpl {
+	return &mailController{db: db}
+}
+
+func (h *mailController) Index(c echo.Context) error {
+	mails := []mdb.Mail{}
+	h.db.
+		Select("id", "created_at", "subject", "text", "html").
+		Preload("FromAddresses").
+		Preload("ToAddresses").
+		Preload("CcAddresses").
+		Preload("BccAddresses").
+		Find(&mails)
+
+	res := make([]response.Mail, len(mails))
+	for i, e := range mails {
+		res[i] = response.Mail{
+			CreatedAt:     e.CreatedAt,
+			FromAddresses: response.ConvertToMailAddresses(e.FromAddresses),
+			ToAddresses:   response.ConvertToMailAddresses(e.ToAddresses),
+			CcAddresses:   response.ConvertToMailAddresses(e.CcAddresses),
+			BccAddresses:  response.ConvertToMailAddresses(e.BccAddresses),
+			Subject:       e.Subject,
+			Text:          e.Text,
+			HTML:          e.HTML,
+		}
+	}
+
+	return c.JSON(http.StatusOK, res)
+}
--- internal/server/http.go
+++ internal/server/http.go
@@ -1,68 +1,23 @@
 package server
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/gommon/log"
-	mdb "github.com/t1732/mmbox/internal/model/db"
-	"github.com/t1732/mmbox/internal/model/response"
 	"gorm.io/gorm"
+
+	"github.com/t1732/mmbox/internal/controller"
 )
-
-type Impl interface {
-	root(c echo.Context) error
-	mails(c echo.Context) error
-}
-
-type Handler struct {
-	db *gorm.DB
-}
-
-func newHandler(db *gorm.DB) Impl {
-	return &Handler{db: db}
-}
 
 func HttpNewServer(db *gorm.DB) *echo.Echo {
 	e := echo.New()
 	e.Logger.SetLevel(log.INFO)
 	e.HideBanner = true
 
-	impl := newHandler(db)
+	rootCtl := controller.NewRootController()
+	e.GET("/", rootCtl.Show)
 
-	e.GET("/", impl.root)
-	e.GET("/mails", impl.mails)
+	mailCtl := controller.NewMialController(db)
+	e.GET("/mails", mailCtl.Index)
 
 	return e
-}
-
-func (h *Handler) root(c echo.Context) error {
-	return c.JSON(http.StatusOK, "Pong")
-}
-
-func (h *Handler) mails(c echo.Context) error {
-	mails := []mdb.Mail{}
-	h.db.
-		Select("id", "created_at", "subject", "text", "html").
-		Preload("FromAddresses").
-		Preload("ToAddresses").
-		Preload("CcAddresses").
-		Preload("BccAddresses").
-		Find(&mails)
-
-	res := make([]response.Mail, len(mails))
-	for i, e := range mails {
-		res[i] = response.Mail{
-			CreatedAt:     e.CreatedAt,
-			FromAddresses: response.ConvertToMailAddresses(e.FromAddresses),
-			ToAddresses:   response.ConvertToMailAddresses(e.ToAddresses),
-			CcAddresses:   response.ConvertToMailAddresses(e.CcAddresses),
-			BccAddresses:  response.ConvertToMailAddresses(e.BccAddresses),
-			Subject:       e.Subject,
-			Text:          e.Text,
-			HTML:          e.HTML,
-		}
-	}
-
-	return c.JSON(http.StatusOK, res)
 }
