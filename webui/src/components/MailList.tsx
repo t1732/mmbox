00b03c7c155218f570ba193565223dc0e95e2a20
t1732 <oo1732oo@gmail.com>
2023-04-13 14:27:46 +0900

    feat: react-query でのデータ取得

--- webui/src/components/MailList.tsx
+++ webui/src/components/MailList.tsx
@@ -1,1 +1,34 @@
-export const MailList = () => <ul />;
+import { useQuery } from '@tanstack/react-query';
+
+type Mail = {
+  messageId: string;
+  subject: string;
+  createdAt: string;
+};
+
+const fetchData = async () => {
+  const res = await fetch('http://localhost:8025/mails');
+
+  return res.json();
+};
+
+export const MailList = () => {
+  const { data, isSuccess } = useQuery<Mail[], Error>({
+    queryKey: ['mails'],
+    queryFn: fetchData,
+  });
+
+  if (!isSuccess) {
+    return <div>no data</div>;
+  }
+
+  return (
+    <div>
+      <ul>
+        {data?.map((mail: Mail) => (
+          <li key={mail.messageId}>{mail.subject} / {mail.createdAt}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
