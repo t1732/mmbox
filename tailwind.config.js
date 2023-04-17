5f02f8be15b90577644ad6cf31ced4cad54aaf48
t1732 <oo1732oo@gmail.com>
2023-04-17 18:47:41 +0900

    feat: tw-elements 入れてみる

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 module.exports = {
-  content: ['./webui/index.html', './webui/src/**/*.{js,jsx,ts,tsx,html}'],
+  content: [
+    './webui/index.html',
+    './webui/src/**/*.{js,jsx,ts,tsx,html}',
+    './node_modules/tw-elements/dist/js/**/*.js',
+  ],
   theme: {
-    extend: {},
+    screens: {},
+    colors: {},
+    fontFamily: {},
   },
-  Darkmode: 'class',
-  plugins: [require('@tailwindcss/forms')],
+  darkmode: 'class',
+  plugins: [require('tw-elements/dist/plugin.cjs')],
 };
