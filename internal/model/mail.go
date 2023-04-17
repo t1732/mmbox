79b2d1302cb53a3bec9a6e1c12d95361617b07d1
t1732 <oo1732oo@gmail.com>
2023-04-17 16:28:45 +0900

    refactor: 不要なカラムの削除

--- internal/model/mail.go
+++ internal/model/mail.go
@@ -17,7 +17,6 @@ type Mail struct {
 	ID         uint
 	CreatedAt  time.Time `gorm:"not null"`
 	Date       string    `gorm:"not null;index:search"`
-	SearchText string    `gorm:"not null"`
 	Source     string    `gorm:"not null"`
 }
 
@@ -29,15 +28,6 @@ func (m *Mail) BeforeSave(tx *gorm.DB) (err error) {
 
 	m.CreatedAt = em.Headers.Date
 	m.Date = em.Headers.Date.Format("2006-01-02")
-	m.SearchText = strings.Join([]string{
-		em.Text,
-		em.HTML,
-		em.Headers.Subject,
-		mailAddressesToString(em.Headers.From),
-		mailAddressesToString(em.Headers.To),
-		mailAddressesToString(em.Headers.Cc),
-		mailAddressesToString(em.Headers.Bcc),
-	}, " ")
 
 	return
 }
