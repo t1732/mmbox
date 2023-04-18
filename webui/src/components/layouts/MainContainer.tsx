fc2fc8c4284524fd87fbfe826f849750fb032f94
t1732 <oo1732oo@gmail.com>
2023-04-18 21:16:18 +0900

    refactor: メインコンテナ幅調整

--- webui/src/components/layouts/MainContainer.tsx
+++ webui/src/components/layouts/MainContainer.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 import { Container } from '@mui/material';
 
 export const MainContainer = ({ children }: { children: ReactNode }) => (
-  <Container maxWidth="md" sx={{ marginTop: '64px' }}>
+  <Container maxWidth="lg" sx={{ marginTop: '64px' }}>
     <main>{children}</main>
   </Container>
 );
