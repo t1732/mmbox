b3c1a7c42e13f63d9b6967ccd8d4e329c988a558
t1732 <oo1732oo@gmail.com>
2023-04-18 13:24:00 +0900

    chore: install mui

--- webui/src/main.tsx
+++ webui/src/main.tsx
@@ -1,24 +1,28 @@
-
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import App from './App';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
-if (
-  localStorage.theme === 'dark' ||
-  (!('theme' in localStorage) &&
-    window.matchMedia('(prefers-color-scheme: dark)').matches)
-) {
-  document.documentElement.classList.add('dark');
-} else {
-  document.documentElement.classList.remove('dark');
-}
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+});
 
 const container = document.getElementById('app');
 if (container !== null) {
   const root = createRoot(container);
   root.render(
     <React.StrictMode>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </React.StrictMode>,
   );
 }
