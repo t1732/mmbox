9d0191a9a8ad20740b08fdbaa489e477b7d94549
t1732 <oo1732oo@gmail.com>
2023-04-20 17:08:29 +0900

    feat: dark mode

--- webui/src/App.tsx
+++ webui/src/App.tsx
@@ -1,15 +1,67 @@
+import { createContext, useMemo, useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { CssBaseline, PaletteMode, useMediaQuery } from '@mui/material';
+import { red } from '@mui/material/colors';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { LayoutWrapper } from './components/LayoutWrapper';
 import { MailBox } from './components/contents/MailBox';
 
 const queryClient = new QueryClient();
+const ColorModeContext = createContext({ toggleColorMode: () => {} });
+const getDesignTokens = (mode: PaletteMode) => ({
+  palette: {
+    mode,
+    ...(mode === 'light'
+      ? {
+          // light mode
+          primary: {
+            main: '#ffffff',
+          },
+          secondary: {
+            main: red[700],
+          },
+        }
+      : {
+          // dark mode
+          primary: {
+            main: '#000000',
+          },
+          secondary: {
+            main: red[400],
+          },
+        }),
+  },
+});
 
-const App = () => (
-  <LayoutWrapper>
-    <QueryClientProvider client={queryClient}>
-      <MailBox />
-    </QueryClientProvider>
-  </LayoutWrapper>
-);
+const App = () => {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const [mode, setMode] = useState<PaletteMode>(
+    prefersDarkMode ? 'dark' : 'light',
+  );
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode: PaletteMode) =>
+          prevMode === 'light' ? 'dark' : 'light',
+        );
+      },
+    }),
+    [],
+  );
+  const theme = useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <LayoutWrapper>
+          <QueryClientProvider client={queryClient}>
+            <MailBox />
+          </QueryClientProvider>
+        </LayoutWrapper>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+};
 
 export default App;
--- webui/src/main.tsx
+++ webui/src/main.tsx
@@ -1,34 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
-import CssBaseline from '@mui/material/CssBaseline';
 import App from './App';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#ffffff",
-    },
-    secondary: {
-      main: red[700],
-    },
-  },
-});
-
 const container = document.getElementById('app');
 if (container !== null) {
   const root = createRoot(container);
   root.render(
     <React.StrictMode>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
+      <App />
     </React.StrictMode>,
   );
 }
