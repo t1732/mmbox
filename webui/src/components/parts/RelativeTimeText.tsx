a92ba522c3cd8d4eb5b9d3d7681f52db233041b6
t1732 <oo1732oo@gmail.com>
2023-04-28 21:38:28 +0900

    refactor: 24H 表示にする

--- webui/src/components/parts/RelativeTimeText.tsx
+++ webui/src/components/parts/RelativeTimeText.tsx
@@ -1,29 +1,30 @@
-import dayjs, { extend } from 'dayjs';
-import relativeTime from 'dayjs/plugin/relativeTime';
 import { Tooltip, Typography } from '@mui/material';
-
-extend(relativeTime);
+import { dayjs } from '../../tools/dayjs';
 
 type Props = {
   time: string;
 };
 
-export const RelativeTimeText = ({ time }: Props) => (
-  <Tooltip title={dayjs(time).format('YYYY/MM/DD hh:mm')}>
-    <Typography
-      variant="caption"
-      component="span"
-      display="block"
-      align="right"
-      gutterBottom
-      sx={{
-        display: 'inline-flex',
-        alignItems: 'center',
-        float: 'right',
-        minWidth: '100px',
-      }}
-    >
-      {dayjs(time).fromNow()}
-    </Typography>
-  </Tooltip>
-);
+export const RelativeTimeText = ({ time }: Props) => {
+  const timeObj = dayjs(time);
+
+  return (
+    <Tooltip title={timeObj.format('YYYY/MM/DD HH:mm')}>
+      <Typography
+        variant="caption"
+        component="span"
+        display="block"
+        align="right"
+        gutterBottom
+        sx={{
+          display: 'inline-flex',
+          alignItems: 'center',
+          float: 'right',
+          minWidth: '100px',
+        }}
+      >
+        {timeObj.fromNow()}
+      </Typography>
+    </Tooltip>
+  );
+};
---
+++ webui/src/tools/dayjs.ts
@@ -0,0 +1,6 @@
+import dayjs, { extend } from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+extend(relativeTime);
+
+export { dayjs };
